e5883934a5b9266f5bc9f38ae8c97a52c98e0704
N739DW <help.unlock@gmail.com>
2017-02-20 17:52:16 -0600

    first commit

---
+++ Script.js
@@ -0,0 +1,11 @@
+
+function call1(){
+	/*alert("fish");
+	document.getElementById('create2').innerHTML="Tock";*/
+	Treater.style.visibility = ('hidden');
+	Pirate.style.visibility = ('visible');
+	var audio = new Audio('audio_file.wav');
+	audio.play();
+}
+
+	
