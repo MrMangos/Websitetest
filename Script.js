abd6ac6da8cbbad2553a805ff5ada0f5b8cd16bb
MrMangos <help.unlock@gmail.com>
2017-02-20 18:42:06 -0600

    Update Script.js

--- Script.js
+++ Script.js
@@ -4,8 +4,8 @@ function call1(){
 	document.getElementById('create2').innerHTML="Tock";*/
 	Treater.style.visibility = ('hidden');
 	Pirate.style.visibility = ('visible');
-	var audio = new Audio('audio_file.wav');
-	audio.play();
+	/*var audio = new Audio('audio_file.wav');*/
+	pirateaudio.play();
 }
 
 	
